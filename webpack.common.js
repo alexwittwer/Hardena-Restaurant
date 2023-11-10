8ebe8e0d6c35e27cfd4b6604205f5af3d9453e31
alexwittwer <wittweral@gmail.com>
2023-11-09 20:52:50 -0500

    actually finish initializing

--- webpack.common.js
+++ webpack.common.js
@@ -7,11 +7,11 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "Production",
+      template: "./src/index.html",
     }),
   ],
   output: {
-    filename: "[name].bundle.js",
+    filename: "App.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
