15a34481c8042442e6017e14b59cbbed9bc2e89d
alexwittwer <wittweral@gmail.com>
2023-11-11 15:44:26 -0500

    Troubleshooting

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import "./input.css";
-import nav from "./modules/nav";
-import home from "./modules/home";
-
-const header = document.querySelector(".header");
-const container = document.querySelector(".container");
-
-header.appendChild(nav(container));
-container.appendChild(home());
--- src/modules/events.js
+++
@@ -1,7 +0,0 @@
-export default function events() {
-  const comp = document.createElement("div");
-
-  comp.textContent = "events";
-
-  return comp;
-}
