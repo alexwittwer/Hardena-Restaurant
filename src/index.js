e425054012157f97a1e6c7effb2078abb0f6f355
alexwittwer <wittweral@gmail.com>
2023-11-11 17:14:43 -0500

    add github link

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import "./input.css";
 import nav from "./modules/nav";
 import home from "./modules/home";
+import Github from "./assets/github.svg";
 
 const header = document.querySelector(".header");
 const container = document.querySelector(".container");
 
 header.appendChild(nav(container));
 container.appendChild(home());
+
+// add github footer
+const footer = document.createElement("footer");
+const link = document.createElement("a");
+
+link.href = "https://github.com/alexwittwer";
+link.target = "_blank";
+link.id = "github-link";
+
+const githubber = new Image();
+githubber.src = Github;
+githubber.classList.add("github-icon");
+
+link.appendChild(githubber);
+footer.appendChild(link);
+document.body.appendChild(footer);
